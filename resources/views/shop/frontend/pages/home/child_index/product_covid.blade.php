849a79d6a00c5c8b625006825b6f8e2ddeca34e9
giangk9a1 <giangk9a1@gmail.com>
2022-08-18 10:27:50 +0700

    Update Trang chủ

--- resources/views/shop/frontend/pages/home/child_index/product_covid.blade.php
+++ resources/views/shop/frontend/pages/home/child_index/product_covid.blade.php
@@ -1,23 +1,25 @@
-<div class="wp-inner">
-    <h1 class="d-flex mb-5">
-        <img src="{{asset('images/shop/covid1.png')}}" alt="" srcset="">
-        <p>Sản phẩm hậu Covid</p>
-    </h1>
-    <ul class="clearfix">
-        @php
-            $items = [
-                [ 'name' => 'Viên sủi opimax Imunity','price'  => '115.000đ','unit'=>'tuýp','image' => 'covid2.png'],
-                [ 'name' => 'Viên sủi opimax Imunity','price'  => '115.000đ','unit'=>'tuýp','image' => 'covid3.png'],
-                [ 'name' => 'Viên sủi opimax Imunity','price'  => '115.000đ','unit'=>'tuýp','image' => 'covid4.png'],
-                [ 'name' => 'Viên sủi opimax Imunity','price'  => '115.000đ','unit'=>'tuýp','image' => 'covid5.png'],
-                [ 'name' => 'Viên sủi opimax Imunity','price'  => '115.000đ','unit'=>'tuýp','image' => 'covid6.png'],
-                [ 'name' => 'Viên sủi opimax Imunity','price'  => '115.000đ','unit'=>'tuýp','image' => 'covid7.png']
-            ];
-        @endphp
-        @foreach ($items as $item)
-            <li>
-                @include("$moduleName.pages.$controllerName.partial.product",['item'=>$item])
-            </li>
-        @endforeach
-    </ul>
+<div id="feature-product-wp">
+    <div class="mb-3 headf position-relative pl-5">
+        <h1>Sản phẩm hậu covid</h1>
+        <img src="{{asset('images/shop/lua1.png')}}" alt="" srcset="">
+    </div>
+    <div class="">
+        <ul class="list-item">
+            @php
+                $items = [
+                    [ 'name' => 'Viên sủi opimax Imunity','price'  => '115.000đ','unit'=>'tuýp','image' => 'covid2.png'],
+                    [ 'name' => 'Viên sủi opimax Imunity','price'  => '115.000đ','unit'=>'tuýp','image' => 'covid3.png'],
+                    [ 'name' => 'Viên sủi opimax Imunity','price'  => '115.000đ','unit'=>'tuýp','image' => 'covid4.png'],
+                    [ 'name' => 'Viên sủi opimax Imunity','price'  => '115.000đ','unit'=>'tuýp','image' => 'covid5.png'],
+                    [ 'name' => 'Viên sủi opimax Imunity','price'  => '115.000đ','unit'=>'tuýp','image' => 'covid6.png'],
+                    [ 'name' => 'Viên sủi opimax Imunity','price'  => '115.000đ','unit'=>'tuýp','image' => 'covid7.png']
+                ];
+            @endphp
+            @foreach ($items as $item)
+                <li>
+                    @include("$moduleName.pages.$controllerName.partial.product",['item'=>$item])
+                </li>
+            @endforeach
+        </ul>
+    </div>
 </div>
