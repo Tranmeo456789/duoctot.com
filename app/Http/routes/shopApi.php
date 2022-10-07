d69b05ceb5fc60425aea57f5e060ddd57bf394e3
giangk9a1 <giangk9a1@gmail.com>
2022-10-07 09:17:21 +0700

    Api get danh sách sản phẩm theo đặc tích sản phẩm và danh mục sản phẩm

--- app/Http/routes/shopApi.php
+++ app/Http/routes/shopApi.php
@@ -1,9 +1,9 @@
 <?php
 $prefixShopApi = '/api';
-Route::group(['prefix' => $prefixShopApi,'namespace' => 'Shop\Api','middleware' => ['jwt.auth']], function () {
+Route::group(['prefix' => $prefixShopApi,'namespace' => 'Shop\Api','middleware' => []], function () {
     Route::get('/get-list-cat-product-level-2', 'CatProductController@index')->name('catProduct.getList');
     Route::get('/get-list-cat-product-by-parent-id/{parent_id}', 'CatProductController@getListByParent')->name('catProduct.getListByParent')->where('parent_id', '[0-9]+');
-    Route::get('/get-list-product/{cat_product_id}', 'ProductController@index')->name('product.getList')->where('cat_product_id', '[0-9]+');
+    Route::get('/get-list-product/{cat_product_id?}', 'ProductController@index')->name('product.getList')->where('cat_product_id', '[0-9]+');
     Route::get('/detail-product/{id}', 'ProductController@detail')->name('product.detail')->where('cat_product_id', '[0-9]+');
+    Route::get('/get-list-product-featurer', 'ProductController@getListFeaturer')->name('product.feather');
 });
-
