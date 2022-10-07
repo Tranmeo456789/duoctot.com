46164efaeba3806025c236a6b57b3df7327a4d2c
giangk9a1 <giangk9a1@gmail.com>
2022-10-07 09:32:32 +0700

    Add middleware cho api

--- app/Http/routes/shopApi.php
+++ app/Http/routes/shopApi.php
@@ -1,6 +1,6 @@
 <?php
 $prefixShopApi = '/api';
-Route::group(['prefix' => $prefixShopApi,'namespace' => 'Shop\Api','middleware' => []], function () {
+Route::group(['prefix' => $prefixShopApi,'namespace' => 'Shop\Api','middleware' => ['jwt.auth']], function () {
     Route::get('/get-list-cat-product-level-2', 'CatProductController@index')->name('catProduct.getList');
     Route::get('/get-list-cat-product-by-parent-id/{parent_id}', 'CatProductController@getListByParent')->name('catProduct.getListByParent')->where('parent_id', '[0-9]+');
     Route::get('/get-list-product/{cat_product_id?}', 'ProductController@index')->name('product.getList')->where('cat_product_id', '[0-9]+');
