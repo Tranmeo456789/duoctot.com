b1f6b4bf608de75e92a39d0f99324de90e63b4b3
Tranmeo456789 <trantrantrantrantran726@gmail.com>
2022-10-19 07:03:25 +0700

    sản phẩm mới xem

--- app/Http/Controllers/Shop/FrontEnd/ProductController.php
+++ app/Http/Controllers/Shop/FrontEnd/ProductController.php
@@ -25,9 +25,24 @@ class ProductController extends ShopFrontEndController
         $data = CatProductModel::all();
         $_SESSION['cat_product']= $catps = data_tree1($data, 0);
     }
-    public function detail(Request $request){
+    public function detail(Request $request){   
         $params['id'] = intval($request->id);
-        $item= $this->model->getItem($params,['task' => 'frontend-get-item']);
+        $item= $this->model->getItem($params,['task' => 'frontend-get-item']);      
+        if(isset($_COOKIE["productNewView"])){
+            $product_new_view=json_decode($_COOKIE["productNewView"],true);
+            if(count($product_new_view) > 8){
+                $item_first=array_keys($product_new_view)[0];
+                unset($product_new_view[$item_first]);
+            }
+        }else{
+            $product_new_view=[];
+        } 
+         $product_new_view[$params['id']]['name']=$item->name;
+         $product_new_view[$params['id']]['price']=$item->price;
+         $product_new_view[$params['id']]['image']=$item->image;
+         $product_new_view[$params['id']]['unit']=$item->unitProduct->name;  
+         $product_new_view=json_encode($product_new_view);  
+        setcookie("productNewView", $product_new_view,time() + config('myconfig.time_cookie'), "/", $_SERVER['SERVER_NAME']);
         return view($this->pathViewController . 'detail',compact('item'));
     }
 }
