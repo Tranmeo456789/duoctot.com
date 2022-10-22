e8b90dee28ba709c5d03a6eddc694712ce09c202
Tranmeo456789 <trantrantrantrantran726@gmail.com>
2022-10-23 02:05:13 +0700

    fix respon giỏ hàng

--- resources/views/shop/frontend/templates/menu_cart.blade.php
+++ resources/views/shop/frontend/templates/menu_cart.blade.php
@@ -16,22 +16,24 @@
 @endphp
 <span class="number_cartmenu {{$classNumberCartMenu}}">{{$number_product}}</span>
 <div class="dropdown-cart">
-<div class="close-cart"><img src="{{asset('images/shop/dn4.png')}}" alt=""></div>
-    <span class="arrow-up"><i class="fas fa-sort-up"></i></span>
-    <p class="text-success noti-cart {{$classMessage}}">{{$message}}</p>
-    <ul class="list-cart-mini">
-        @foreach ($itemsCart as $keyCart =>$valCart)
-        <li class="user-seller user-seller-{{$keyCart}}" data-id={{$keyCart}}>
-            <p class="name_seller">{!! $valCart['name'] !!}</p>
-            <ul class="list-product-in-cart">
-                @foreach ($valCart['product'] as $product)
-                    @include ("$moduleName.pages.cart.partial.item_product_in_cart_mini",['item' => $product,'user_sell' => $keyCart])
-                @endforeach
-            </ul>
-            <div class="text-right"><a href="{{route('fe.product.viewcart',['user_sell' => $keyCart])}}" class="btn btn-sm btn-success btn-view-cart">Mua hàng</a></div>
-        </li>
-        @endforeach
-    </ul>
-    <div class="text-center"><a href="{{route('fe.product.cart')}}" class="viewcartmini">Xem giỏ hàng</a></div>
-    <div class="cbh2"><a href="{{route('home')}}">Tiếp tục mua hàng</a></div>
+    <div class="position-relative">
+        <div class="close-cart"><img src="{{asset('images/shop/dn4.png')}}" alt=""></div>
+        <span class="arrow-up"><i class="fas fa-sort-up"></i></span>
+        <p class="text-success noti-cart {{$classMessage}}">{{$message}}</p>
+        <ul class="list-cart-mini">
+            @foreach ($itemsCart as $keyCart =>$valCart)
+            <li class="user-seller user-seller-{{$keyCart}}" data-id={{$keyCart}}>
+                <p class="name_seller">{!! $valCart['name'] !!}</p>
+                <ul class="list-product-in-cart">
+                    @foreach ($valCart['product'] as $product)
+                        @include ("$moduleName.pages.cart.partial.item_product_in_cart_mini",['item' => $product,'user_sell' => $keyCart])
+                    @endforeach
+                </ul>
+                <div class="text-right"><a href="{{route('fe.product.viewcart',['user_sell' => $keyCart])}}" class="btn btn-sm btn-success btn-view-cart">Mua hàng</a></div>
+            </li>
+            @endforeach
+        </ul>
+        <div class="text-center"><a href="{{route('fe.product.cart')}}" class="viewcartmini">Xem giỏ hàng</a></div>
+        <div class="cbh2"><a href="{{route('home')}}">Tiếp tục mua hàng</a></div>
+    </div>  
 </div>
