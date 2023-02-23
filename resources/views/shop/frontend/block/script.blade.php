51d38783ae55e9dbf362e188e3c3c8ada565f83f
giangk9a1 <giangk9a1@gmail.com>
2023-02-23 09:28:58 +0700

    Fix code

--- resources/views/shop/frontend/block/script.blade.php
+++ resources/views/shop/frontend/block/script.blade.php
@@ -2,14 +2,11 @@
 <script src="{{ asset('/shop/frontend/js/jquery-3.1.1.js')}}"></script>
 <script src="{{ asset('/shop/frontend/js/jquery.validate.min.js')}}" type="text/javascript"></script>
 <script src="{{ asset('/shop/frontend/js/additional-methods.min.js')}}" type="text/javascript"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js"></script>
+<script src="{{ asset('/shop/template/js/popper.min.js')}}" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="{{ asset('/shop/template/js/bootstrap.min.js')}}"></script>
 
-<script src="{{ asset('/shop/frontend/js/lightslider.js')}}?t=@php echo time() @endphp" type="text/javascript"></script>
-<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+<script src="{{ asset('/shop/frontend/js/lightslider.js')}}" type="text/javascript"></script>
+<script src="{{ asset('/shop/template/js/select2.full.min.js')}}"></script>
 <script src="{{ asset('/shop/frontend/js/main.js')}}?t=@php echo time() @endphp" type="text/javascript"></script>
-
-<script src="{{ asset('/shop/frontend/js/owl.carousel.js')}}?t=@php echo time() @endphp" type="text/javascript"></script>
-
-
+<script src="{{ asset('/shop/frontend/js/owl.carousel.js')}}" type="text/javascript"></script>
 <script src="{{ asset('/shop/frontend/js/my-js.js')}}?t=@php echo time() @endphp" type="text/javascript"></script>
