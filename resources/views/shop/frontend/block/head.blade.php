48fdc4b65bf654dd140d65bcfd13ee1a4b84ee5f
DESKTOP-Q2VSK4R\MANH <trantrantrantrantran726@gmail.com>
2023-03-12 22:16:31 +0700

    fix height dưới banner home

--- resources/views/shop/frontend/block/head.blade.php
+++ resources/views/shop/frontend/block/head.blade.php
@@ -3,10 +3,10 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <link rel="shortcut icon" href="favicon.ico">
-<link href="{{ asset('/shop/template/css/bootstrap.min.css')}}">
-<link href="{{ asset('/shop/template/css/all.min.css') }}"/>
-<link rel="stylesheet" href="{{ asset('/shop/template/css/solid.min.css')}}">
-<link rel="stylesheet" href="{{ asset('shop/template/css/select2.min.css')}}?t=@php echo time() @endphp">
+<link href="{{ asset('/shop/template/css/bootstrap.min.css')}}?t=@php echo time() @endphp" rel="stylesheet" type="text/css"/>
+<link href="{{ asset('/shop/template/css/all.min.css')}}?t=@php echo time() @endphp" rel="stylesheet" type="text/css"/>
+<link rel="stylesheet" href="{{ asset('/shop/template/css/solid.min.css')}}?t=@php echo time() @endphp" rel="stylesheet" type="text/css"/>
+<link rel="stylesheet" href="{{ asset('shop/template/css/select2.min.css')}}?t=@php echo time() @endphp" rel="stylesheet" type="text/css"/>
 <link href="{{ asset('/shop/frontend/css/import/header.css')}}?t=@php echo time() @endphp" rel="stylesheet" type="text/css"/>
 <link href="{{ asset('/shop/frontend/css/import/home.css')}}?t=@php echo time() @endphp" rel="stylesheet" type="text/css"/>
 <link href="{{ asset('/shop/frontend/css/import/cat_product.css')}}?t=@php echo time() @endphp" rel="stylesheet" type="text/css"/>
--- resources/views/shop/frontend/pages/home/index.blade.php
+++ resources/views/shop/frontend/pages/home/index.blade.php
@@ -1,70 +1,69 @@
 @extends('shop.layouts.frontend')
 
 @section('content')
-    <div class="section" id="slider-wp">
-        <div class="section-detail">
-            <div class="item">
-                <img src="{{asset('images/shop/banner1.png')}}" alt="" class="img-fluid" style="width:100%">
-            </div>
-            <div class="item">
-                <img src="{{asset('images/shop/banner2.png')}}" alt="" class="img-fluid" style="width:100%">
-            </div>
-            <div class="item">
-                <img src="{{asset('images/shop/banner3.png')}}" alt="" class="img-fluid" style="width:100%">
+<div class="section" id="slider-wp">
+    <div class="section-detail">
+        <div class="item">
+            <img src="{{asset('images/shop/banner1.png')}}" alt="" class="img-fluid" style="width:100%">
+        </div>
+        <div class="item">
+            <img src="{{asset('images/shop/banner2.png')}}" alt="" class="img-fluid" style="width:100%">
+        </div>
+        <div class="item">
+            <img src="{{asset('images/shop/banner3.png')}}" alt="" class="img-fluid" style="width:100%">
+        </div>
+    </div>
+</div>
+<div class="wp-inner">
+    <div id="hisd" class="position-relative">
+        <div class="d-flex justify-content-center">
+            <div id="form-search" class="d-flex justify-content-center">
+                @include("$moduleName.pages.$controllerName.child_index.search")
             </div>
         </div>
     </div>
+    <div id="buy-medicine" class="mt-3">
+        @include("$moduleName.pages.$controllerName.child_index.buy_medicine")
+    </div>
+    <div id="featured-category" class="mt-5">
+        @include("$moduleName.pages.$controllerName.child_index.featured_category")
+    </div>
+</div>
+<div id="productsl" class="mt-5 py-4 product_hcovid">
     <div class="wp-inner">
-        <div id="hisd" class="position-relative">
-            <div class="d-flex justify-content-center">
-                <div id="form-search" class="d-flex justify-content-center">
-                    @include("$moduleName.pages.$controllerName.child_index.search")
-                </div>
-            </div>
-        </div>
-        <div id="buy-medicine">
-            @include("$moduleName.pages.$controllerName.child_index.buy_medicine")
-        </div>
-        <div id="featured-category" class="mt-5">
-            @include("$moduleName.pages.$controllerName.child_index.featured_category")
-        </div>
+        @include("$moduleName.pages.$controllerName.child_index.product_covid")
     </div>
-    <div id="productsl" class="mt-5 py-4 product_hcovid">
-        <div class="wp-inner">
-            @include("$moduleName.pages.$controllerName.child_index.product_covid")
-        </div>
+</div>
+<div class="wp-inner mt-5">
+    <div id="selling-product">
+        @include("$moduleName.templates.selling_product")
     </div>
-    <div class="wp-inner mt-5">
-        <div id="selling-product">
-            @include("$moduleName.templates.selling_product")
-        </div>
-        <div id="selling-product" class="mt-5">
-            @include("$moduleName.pages.$controllerName.child_index.product_in_object")
-        </div>
+    <div id="selling-product" class="mt-5">
+        @include("$moduleName.pages.$controllerName.child_index.product_in_object")
     </div>
-    <!-- <div id="qadoctor" class="mt-3 pb-5">
+</div>
+<!-- <div id="qadoctor" class="mt-3 pb-5">
         @include("$moduleName.pages.$controllerName.child_index.qadoctor")
     </div> -->
-    <div class="wp-inner">
-        @include("$moduleName.pages.$controllerName.child_index.news")
-    </div>
-    <!-- <div class="info-tdoctor mt-5">
+<div class="wp-inner">
+    @include("$moduleName.pages.$controllerName.child_index.news")
+</div>
+<!-- <div class="info-tdoctor mt-5">
         @include("$moduleName.templates.info_tdoctor")
     </div> -->
-    <!-- <div class="wp-inner mt-5">
+<!-- <div class="wp-inner mt-5">
         @include("$moduleName.templates.info_app")
     </div> -->
-    <div class="service-tdoctor mt-5">
-        @include("$moduleName.templates.info_service")
+<div class="service-tdoctor mt-5">
+    @include("$moduleName.templates.info_service")
+</div>
+<div class="local">
+    @include("$moduleName.templates.local_drugstore")
+</div>
+<div class="wp-inner mt-5">
+    <div class="feedback-customer">
+        @include("$moduleName.templates.feedback_customer")
     </div>
-    <div class="local">
-        @include("$moduleName.templates.local_drugstore")
-    </div>
-    <div class="wp-inner mt-5">
-        <div class="feedback-customer">
-            @include("$moduleName.templates.feedback_customer")
-        </div>
-    </div>
-    <div class="lc-mask-search"></div>
+</div>
+<div class="lc-mask-search"></div>
 @endsection
-
