f6980d672af7a24f2227915453f1cdde2569a944
DESKTOP-OBK9B4T\trant <trantrantrantrantran726@gmail.com>
2024-01-08 06:35:41 +0700

    shop edit for create_by

--- resources/views/shop/backend/pages/trademark/list.blade.php
+++ resources/views/shop/backend/pages/trademark/list.blade.php
@@ -18,10 +18,12 @@
             <th scope="row" style="width: 10%">{{$temp}}</th>
             <td style="width: 70%" class='name'>{{$val->name}}</td>
             <td style="width: 20%">
+                @if(Session::has('user') && Session::get('user')['user_id'] == $val['created_by'])
                 <a href="{{route("$controllerName.edit",$val->id)}}" class="btn btn-success btn-sm rounded-0 text-white" type="button" data-toggle="tooltip" data-placement="top" title="Sửa"><i class="fa fa-edit"></i></a>
                 <a data-href="{{route("$controllerName.delete",$val->id)}}" class="btn btn-sm btn-danger btn-delete text-white" data-id="{{$val->id}}" data-toggle="tooltip" data-placement="top" title="Xóa"  data-token="{{csrf_token()}}" >
                     <i class="fa fa-trash"></i>
                 </a>
+                @endif
             </td>
         </tr>
         @endforeach
