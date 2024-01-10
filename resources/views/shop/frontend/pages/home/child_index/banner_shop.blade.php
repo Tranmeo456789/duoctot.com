0b3570e31995944837974f0e215ec1af72516476
DESKTOP-OBK9B4T\trant <trantrantrantrantran726@gmail.com>
2024-01-10 15:47:48 +0700

    show banner QuangCao

--- resources/views/shop/frontend/pages/home/child_index/banner_shop.blade.php
+++ resources/views/shop/frontend/pages/home/child_index/banner_shop.blade.php
@@ -8,34 +8,29 @@
 @if (count($itemsQuangCao) > 0)
     @foreach($itemsQuangCao as $val)
         @php
-            $urlImgDesktop = 'https:://tdoctor.vn/public/fileUpload/quangCao/'  . $val['banner_shop'];
-            $urlImgMobile= 'https:://tdoctor.vn/public/fileUpload/quangCao/' . $val['banner_mobile'];
+            $urlImgDesktop = 'https://tdoctor.vn/public/fileUpload/quangCao/'  . $val['banner_shop'];
+            $urlImgMobile= 'https://tdoctor.vn/public/fileUpload/quangCao/' . $val['banner_mobile'];
             $address = $val['url']??'#';
 
-            $xhtmlDesktop .= "<a href='". $address ."'>
-                                    <li class='item_banner_home text-center'>
+            $xhtmlDesktop .= "<a class='item d-block' href='". $address ."'>
+                                    <div class='item_banner_home text-center'>
                                         <img style='width:100%' src='" . $urlImgDesktop . "' alt=''>
-                                    </li>
+                                    </div>
                                 </a>";
-            $xhtmlMobile .= "<a href='". $address ."'>
-                            <li class='item_banner_home text-center'>
+            $xhtmlMobile .= "<a class='item d-block' href='". $address ."'>
+                            <div class='item_banner_home text-center'>
                                 <img style='width:100%' src='" . $urlImgMobile . "' alt=''>
-                            </li>
+                            </div>
                         </a>";
         @endphp
     @endforeach
-    <div class="row">
-        <div class="col-12">
-            <ul class="banner_doitac banner_doitac_desktop">
-                {!! $xhtmlDesktop !!}
-            </ul>
+    
+    <div id="banner_doitac">
+        <div class="banner_doitac d-none d-md-block">
+        {!! $xhtmlDesktop !!}
         </div>
-    </div>
-    <div class="row">
-        <div class="col-12">
-            <ul class="banner_doitac banner_doitac_mobile">
-                {!! $xhtmlDesktop !!}
-            </ul>
+        <div class="banner_doitac d-block d-md-none">
+        {!! $xhtmlMobile !!}
         </div>
     </div>
 @endif
