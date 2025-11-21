224faa8945be3a421e7677aa9999c73b00bb2574
DESKTOP-D5SAMPN\trant <trantrantrantrantran726@gmail.com>
2025-11-21 23:54:51 +0700

    add image

--- resources/views/shop/frontend/pages/home/child_index/info_page.blade.php
+++ resources/views/shop/frontend/pages/home/child_index/info_page.blade.php
@@ -23,35 +23,35 @@
     <div class="has-bg clr row">
         <div class="span col-4 col-sm-2 m alone text-center mb-3">
             <a href="{{route('fe.product.listPhongKham')}}">
-                <h5 class="counter ctr1">5380</h5>
+                <h5 class="counter ctr1">5.450</h5>
                 <em>Phòng khám</em>
             </a>
         </div>
         <div class="span col-4 col-sm-2 m alone text-center mb-3">
             <a href="{{route('fe.product.listBacSi')}}">
-                <h5 class="counter ctr2">3530</h5>
+                <h5 class="counter ctr2">3.530</h5>
                 <em>Bác sĩ</em>
             </a>
         </div>
         <div class="span col-4 col-sm-2 m alone text-center mb-3">
             <a href="{{route('fe.product.listDrugstore')}}">
-                <h5 class="counter ctr3">20000</h5>
+                <h5 class="counter ctr3">26.020</h5>
                 <em>Nhà thuốc</em>
             </a>
         </div>
         <div class="span col-4 col-sm-2 m alone text-center mb-3">
-            <h5 class="counter ctr4">31856</h5>
+            <h5 class="counter ctr4">2,3 triệu</h5>
             <em>Bệnh nhân</em>
         </div>
         <div class="span col-4 col-sm-2 m alone text-center mb-3">
             <a href="{{route('fe.product.listNhaCungCap')}}">
-                <h5 class="counter ctr4">200</h5>
+                <h5 class="counter ctr4">510</h5>
                 <em>Nhà cung cấp</em>
             </a>
         </div>
         <div class="span col-4 col-sm-2 m alone text-center mb-3">
             <a href="">
-                <h5 class="counter ctr4">9000</h5>
+                <h5 class="counter ctr4">9.100</h5>
                 <em>Sản phẩm</em>
             </a>
         </div>
